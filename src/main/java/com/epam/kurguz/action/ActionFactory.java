306b500df253a1b1fdb0fc0efba1d0acc3699bd8
Евгений <steppe90@mail.ru>
2014-08-12 22:28:45 +0600

    index2

--- src/main/java/com/epam/kurguz/action/ActionFactory.java
+++ src/main/java/com/epam/kurguz/action/ActionFactory.java
@@ -1,15 +1,38 @@
 package com.epam.kurguz.action;
 
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class ActionFactory {
-    private final static Map<String, Action> actions = new HashMap<String, Action>();
+    private static Logger logger = LoggerFactory.getLogger(ActionFactory.class);
+    static Map<String, Action> actionMap = new HashMap<String, Action>();
 
     static {
+        Reflections reflections = new Reflections(ActionFactory.class.getPackage().getName());
+        Set<Class<? extends Action>> actions = reflections.getSubTypesOf(Action.class);
+        for (Class<? extends Action> action : actions) {
+            try {
+                actionMap.put(action.getSimpleName().toLowerCase(), action.newInstance());
+            } catch (InstantiationException e) {
+                logger.error(e.toString());
+            } catch (IllegalAccessException e) {
+                logger.error(e.toString());
+            }
+        }
     }
 
     public static Action getAction(String actionName) {
-        return actions.get(actionName);
+        return actionMap.get(actionName);
+    }
+
+    public static void getAllAction (){
+        for (String s : actionMap.keySet()) {
+            System.out.println(s);
+        }
     }
 }
