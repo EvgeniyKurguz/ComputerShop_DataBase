d55ac87e65718fdce68225b0f576a69342569873
EvgeniyKurguz <steppe90@mail.ru>
2014-08-12 18:10:30 +0600

    index2

--- src/main/java/com/epam/kurguz/servlet/Controller.java
+++ src/main/java/com/epam/kurguz/servlet/Controller.java
@@ -13,5 +13,6 @@ public class Controller extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/index.jsp").forward(request, response);
+
     }
 }
